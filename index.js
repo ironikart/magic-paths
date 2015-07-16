05f26ace92b56a12af2413c510cf43b0110789a1
Anthony Barnes <abarnes@squiz.com.au>
2015-07-17 09:27:49 +1000

    Removed global defaults, doesn't make sense and over complicates things

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 'use strict';
 var glob = require('glob');
 var path = require('path');
-var setDefaults = require('lodash.defaults');
+var merge = require('deep-extend');
 var Promise = require('bluebird');
-var globalDefaults = {
-    prefixes: {
-        cwd: process.cwd()
-    },
-    glob: {}
-};
 
 function makeExpr(prefixes) {
     return new RegExp('^\\s*('+Object.keys(prefixes).join('|')+'):([^\\s]+)\\s*', 'i');
@@ -29,29 +23,15 @@ var prefix = module.exports.prefix = function(pattern, prefixes) {
     return [pattern];
 };
 
-module.exports.defaults = function(options) {
-    globalDefaults = setDefaults(options, globalDefaults);
-};
-
-module.exports.expand = function(pattern) {
-    var cb;
-    var options = globalDefaults;
-
-    // Allow an optional number of arguments & optional callback
+module.exports.expand = function (pattern, options) {
     var args = Array.prototype.slice.call(arguments);
-    if (args.length === 2) {
-        var last = args.pop();
-        if (typeof last === 'function') {
-            cb = last;
-        } else {
-            options = last;
-        }
-    } else if (args.length === 3) {
-        cb = args[2];
-        options = args[1];
-    }
+    var cb = args.length === 3 ? args.pop() : function(){};
 
-    options = setDefaults(options, globalDefaults);
+    options = merge({
+        prefixes: {
+            cwd: process.cwd()
+        }
+    }, options);
 
     return new Promise(function(resolve, reject) {
         var prefixed = prefix(pattern, options.prefixes);
@@ -67,9 +47,7 @@ module.exports.expand = function(pattern) {
                 return file;
             });
             resolve(files);
-            if (cb) {
-                cb(err, files);
-            }
+            cb(err, files);
         });
     });
 };
--- test/spec.js
+++ test/spec.js
@@ -5,27 +5,24 @@ var path = require('path');
 var Promise = require('bluebird');
 
 describe('Magic path expander', function() {
+    var options = {
+        prefixes: {
+            'rel_fixture': 'test/fixtures',
+            'abs_fixture': path.join(process.cwd(), 'test/fixtures'),
+
+            'modules': [
+                'test/fixtures/modules',
+                'test/fixtures/alt_modules'
+            ]
+        }
+    };
+
     it('can be required without throwing', function() {
         this.magic = require('../index.js');
     });
 
-    it('can handle global default configuration', function() {
-        // Set defaults
-        this.magic.defaults({
-            prefixes: {
-                'rel_fixture': 'test/fixtures',
-                'abs_fixture': path.join(process.cwd(), 'test/fixtures'),
-
-                'modules': [
-                    'test/fixtures/modules',
-                    'test/fixtures/alt_modules'
-                ]
-            }
-        });
-    });
-
     it('can expand relative prefixes (cwd)', function(done) {
-        this.magic.expand('rel_fixture:*.html', function(err, files) {
+        this.magic.expand('rel_fixture:*.html', options, function(err, files) {
             expect(err).be.equal(null);
             expect(files.length).to.be.equal(3);
             expect(path.isAbsolute(files[0])).to.be.equal(true);
@@ -34,7 +31,7 @@ describe('Magic path expander', function() {
     });
 
     it('can expand absolute prefixes (cwd)', function(done) {
-        this.magic.expand('abs_fixture:*.html', function(err, files) {
+        this.magic.expand('abs_fixture:*.html', options, function(err, files) {
             expect(err).be.equal(null);
             expect(files.length).to.be.equal(3);
             done();
@@ -42,30 +39,17 @@ describe('Magic path expander', function() {
     });
 
     it('can expand an array of prefixes', function(done) {
-        this.magic.expand('modules:*.html', function(err, files) {
+        this.magic.expand('modules:*.html', options, function(err, files) {
             expect(err).be.equal(null);
             expect(files.length).to.be.equal(2);
             done();
         });
     });
 
-    it('can take custom options', function(done) {
-        this.magic.expand('custom:*.html', {
-            prefixes: {
-                custom: 'test/fixtures/custom'
-            }
-        }, function(err, files) {
-            expect(err).be.equal(null);
-            expect(files.length).to.be.equal(1);
-            expect(files[0].split('/').pop()).to.be.equal('custom.html');
-            done();
-        });
-    });
-
     it('works as a promise', function(done) {
         Promise.join(
-            this.magic.expand('modules:*.html'),
-            this.magic.expand('rel_fixture:*.html')
+            this.magic.expand('modules:*.html', options),
+            this.magic.expand('rel_fixture:*.html', options)
         ).then(function(results) {
             expect(results[0].length).to.be.equal(2);
             expect(results[1].length).to.be.equal(3);
